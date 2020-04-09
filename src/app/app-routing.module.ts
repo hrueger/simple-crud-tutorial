ffbc79d3cedd60e4813281875a623e9848642da3
H. Rüger <hannesrueger@gmx.de>
2020-04-08 21:06:25 -1000

    added the two basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListBooksComponent } from './components/list-books/list-books.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "listBooks",
+    component: ListBooksComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/home"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListBooksComponent } from './components/list-books/list-books.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListBooksComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
